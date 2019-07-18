18d82092cba0f1e81674d2e6b1af1699079246f0
Oluseun <oluseun@comatap.com>
2019-07-18 13:38:07 +0900

    Delete Existing Data

--- sensors/views.py
+++ sensors/views.py
@@ -134,6 +134,7 @@ def combined_data(request, pk):
     sensor_id = sensor.id
 
     data = Data.objects.filter(sensor=sensor)
+    data.delete()
     
 
     # get all the available data on this sensor
